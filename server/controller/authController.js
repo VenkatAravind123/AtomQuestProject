12717a4a5b33d0efb508c3dafad1b756e2fd9c4f
Venkat Aravind <venkataravind.123456@gmail.com>
2026-05-17 20:28:19 +0530

    cookie updated

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -21,7 +21,7 @@ export const login = async (req, res) => {
   const token = signToken(user);
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
